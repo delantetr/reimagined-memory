f690af2ed7c2ccb8f8c4b0af721ca1c8426efe5a
Delante Randolph <delantetr@Delante-MacBook-Pro.local>
2023-06-13 08:56:14 -0400

    new/changed code. updated

--- Assets/script.js
+++ Assets/script.js
@@ -11,33 +11,56 @@
 
 var questions = [
   {
-    question: "What is the capital of France?",
-    options: ["Paris", "London", "Rome", "Berlin"],
-    correctAnswer: "Paris"
+    question: "What does CSS stand for?",
+    options: ["Cascading Style Sheet", "Controlled Style Sheet", "Carbon Steel Side", "Cascading Score Sheet"],
+    correctAnswer: "Cascading Style Sheet"
   },
   {
-    question: "What is the largest planet in our solar system?",
-    options: ["Mars", "Jupiter", "Saturn", "Venus"],
-    correctAnswer: "Jupiter"
+    question: "What does HTML stand for?",
+    options: ["Hardtime Money Language", "Hypertext Markup Language", "Hypertext Markdown Language", "Hyperlink Marking Language"],
+    correctAnswer: "Hypertext Markup Language"
+  },
+  {
+    question: "What is the correct syntax to declare a JavaScript variable?",
+    options: ["var carName;", "variable carName;", "v carName;"],
+    correctAnswer: "var carName;"
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    options: ["<js>", "<scripting>", "<script>"],
+    correctAnswer: "<script>"
+  },
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    options: ["The <head> section", "The <body> section", "Both the <head> and <body> section"],
+    correctAnswer: "Both the <head> and <body> section"
   }
 ];
 
 
 
 // DOM Navigation
-var quiz = document.getElementById('main');
+var quiz = document.getElementById('quiz-window');
+var startWindow = document.getElementById("start-window");
 var startButton = document.getElementById("start-button");
 var questionWindow = document.getElementById('questions');
 var answersWindow = document.getElementById('answers');
 var feedbackWindow = document.getElementById('feedback');
-var timerEl = document.getElementById("timer-count");
-var scoreWindow = document.getElementById('score');
+var timerEl = document.getElementById("time-remaining");
+var resultWindow = document.getElementById("result-window");
+var scoreWindow = document.getElementById('final-score');
+var scoreForm = document.getElementById("score-form");
+var initialsContainer = document.getElementById('initials');
+// var viewHighscoresButton = document.getElementById('view-highscores');
+var highscoresList = document.getElementById('highscores-list');
+var highscoresWindow = document.getElementById('highscores-container');
 
 
 var currentQuestionIndex = 0;
 var score = 0;
 var timeRemaining = 75;
 var timer;
+var scores = [];
 
 
 
@@ -45,6 +68,8 @@ var timer;
 function startGame() {
     // Test to make sure the click is being recieved.
     console.log("Click Confirmed");
+    startWindow.style.display = "none";
+    quiz.style.display = "block";
     startTimer()
     showQuestion()
   }
@@ -53,12 +78,14 @@ startButton.addEventListener("click", startGame);
 
 // 2.--------------------------------------------------------------
 function startTimer() {
-  var timer = setInterval(updateTimer, 1000)
-}
+  timerInterval = setInterval(function() {
+    timeRemaining--;
+    timerEl.textContent = timeRemaining;
 
-function updateTimer() {
-timeRemaining--;
-timerEl.textContent = timeRemaining;
+    if (timeRemaining <= 0) {
+      endQuiz();
+    }
+  }, 1000);
 }
 
 // 3.---------------------------------------------------------------
@@ -71,22 +98,28 @@ function showQuestion() {
 
   // Create option buttons for the current question
   for (let i = 0; i < currentQuestion.options.length; i++) {
-    var optionButton = document.createElement('button');
-    optionButton.textContent = currentQuestion.options[i];
-    optionButton.addEventListener('click', checkAnswer);
-    answersWindow.appendChild(optionButton);
+    var choice = currentQuestion.options[i];
+    var li = document.createElement("li");
+    var button = document.createElement("button");
+
+    button.textContent = choice;
+    button.setAttribute("data-index", i);
+    button.addEventListener("click", checkAnswer);
+
+    li.appendChild(button);
+    answersWindow.appendChild(li);
   }
 }
 
 // 4.--------------------------------------------------------------
 function checkAnswer(event) {
-  var selectedAnswer = event.target;
+  var selectedChoiceIndex = parseInt(this.getAttribute("data-index"));
   var currentQuestion = questions[currentQuestionIndex];
 
-  if (selectedAnswer.textContent === currentQuestion.correctAnswer) {
-    score++
+  if (currentQuestion.options[selectedChoiceIndex] === currentQuestion.correctAnswer) {
+    score++;
     feedbackWindow.textContent = "Correct!";
-  } 
+  }
 
 // 5.---------------------------------------------------------------
   else {
@@ -96,27 +129,60 @@ function checkAnswer(event) {
 
 // 6.---------------------------------------------------------------
   currentQuestionIndex++;
-  if (currentQuestionIndex === questions.length) {
-    endQuiz();
-  }
-  else {
+  if (currentQuestionIndex < questions.length) {
     showQuestion();
+  } else {
+    endQuiz();
   }
 }
 
 // 7.----------------------------------------------------------------
 function endQuiz() {
-  clearInterval(timer);
   quiz.style.display = 'none';
+  resultWindow.style.display = "block";
+  timerEl.style.display = 'none';
+  scoreWindow.textContent = score;
 
-  answersWindow.textContent = 'Your Score' + score;
-
-  var initials = prompt('Please enter your initials:');
-  saveScore(initials, score);
+  scoreForm.addEventListener("submit", saveScore);
 }
 
 // 8.---------------------------------------------------------------
-function saveScore(initials, score) {
-  var scoreData = {initials, score};
-  localStorage.setItem('quizScore', scoreData);
+function saveScore(event) {
+  event.preventDefault();
+
+  initialsContainer.value.toUpperCase();
+  // Save initials and score to storage or perform any desired action here
+  var initials = document.querySelector(initialsContainer.value);
+  localStorage.setItem("initials", initials);
+  // Example code to display the saved score:
+  var highScoresElement = document.createElement("p");
+  initialsContainer = initials + ": " + score;
+  highscoresWindow.appendChild(highScoresElement);
+
+  var scoreForm = document.getElementById("score-form");
+  scoreForm.style.display = "none";
 }
+
+
+
+// Function to print high scores
+// function printHighScores() {
+//   highscoresWindow.style.display = 'block';
+//   highscoresList.innerHTML = '';
+
+//   // Retrieve scores from localStorage
+//   getScoresFromLocalStorage();
+
+//   // Sort scores in descending order
+//   scores.sort((a, b) => b.score - a.score);
+
+//   // Display the high scores
+//   for (var i = 0; i < scores.length; i++) {
+//     var scoreItem = document.createElement('li');
+//     scoreItem.textContent = scores[i].initials + ' - ' + scores[i].score;
+//     highscoresList.appendChild(scoreItem);
+//   }
+
+//   viewHighscoresButton.addEventListener('click', printHighScores);
+
+// }
